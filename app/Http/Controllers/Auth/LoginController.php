abd4282d4eb1bf1d5e7fa740f35cbe4103cfd2f5
Pixlforge <celien@pixlforge.ch>
2018-11-18 19:30:40 +0100

    wip

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -18,7 +18,7 @@ class LoginController extends Controller
     {
         $token = auth('api')->attempt($request->only('email', 'password'));
 
-        return $token;
+        return response($token);
 
         if (!$token) {
             return response([
