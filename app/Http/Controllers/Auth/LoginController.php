49cbcbbea21999a914cb809883d3b841d7c8debf
Pixlforge <celien@pixlforge.ch>
2018-11-18 20:00:23 +0100

    wip

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\PrivateUserResource;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class LoginController extends Controller
 {
@@ -16,15 +18,31 @@ class LoginController extends Controller
      */
     public function __invoke(LoginRequest $request)
     {
-        $token = auth()->attempt($request->only('email', 'password'));
-
-        if (!$token) {
+        $credentials = $request->only('email', 'password');
+        
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response([
+                    'errors' => [
+                        'email' => ['Could not sign you in with the credentials provided.']
+                    ]
+                ], 422);
+            }
+        } catch (JWTException $e) {
             return response([
-                'errors' => [
-                    'email' => ['Could not sign you in with the credentials provided.']
-                ]
-            ], 422);
+                'errors' => 'could_not_create_token'
+            ], 500);
         }
+
+        // $token = auth()->attempt($request->only('email', 'password'));
+
+        // if (!$token) {
+        //     return response([
+        //         'errors' => [
+        //             'email' => ['Could not sign you in with the credentials provided.']
+        //         ]
+        //     ], 422);
+        // }
 
         return (new PrivateUserResource($request->user()))
             ->additional([
