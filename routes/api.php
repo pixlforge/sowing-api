88d125e1881f94ea14d9b181b682480c413a59b7
Pixlforge <celien@pixlforge.ch>
2018-12-06 12:32:11 +0100

    Simplified route names

--- routes/api.php
+++ routes/api.php
@@ -22,27 +22,19 @@ Route::prefix('/auth')->namespace('Auth')->name('auth.')->group(function () {
  * Cart
  */
 Route::namespace('Cart')->group(function () {
-    Route::resource('/cart', 'CartController')->names([
-        'store' => 'cart.store',
-    ]);
+    Route::resource('/cart', 'CartController');
 });
 
 /**
  * Categories
  */
 Route::namespace('Categories')->group(function () {
-    Route::resource('/categories', 'CategoryController')->names([
-        'index' => 'categories.index',
-        'show' => 'categories.show',
-    ]);
+    Route::resource('/categories', 'CategoryController');
 });
 
 /**
  * Products
  */
 Route::namespace('Products')->group(function () {
-    Route::resource('/products', 'ProductController')->names([
-        'index' => 'products.index',
-        'show' => 'products.show',
-    ]);
+    Route::resource('/products', 'ProductController');
 });
