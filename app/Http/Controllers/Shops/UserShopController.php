e53d1ab12265118218aad4e001bd2d79d45fbbc2
Pixlforge <celien@pixlforge.ch>
2019-01-28 18:01:12 +0100

    Eager load the country relationship

--- app/Http/Controllers/Shops/UserShopController.php
+++ app/Http/Controllers/Shops/UserShopController.php
@@ -28,7 +28,7 @@ class UserShopController extends Controller
             return response(null, 204);
         }
 
-        $shop = $request->user()->shop;
+        $shop = $request->user()->shop->with(['country'])->first();
         
         return new ShopResource($shop);
     }
