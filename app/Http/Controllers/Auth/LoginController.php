ecc75e27f0c3f72f4f10abd3d729c74f92743e8c
Pixlforge <celien@pixlforge.ch>
2019-04-02 12:04:11 +0200

    Added login request throttling

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,12 +2,32 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Lang;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Resources\Users\PrivateUserResource;
+use Illuminate\Foundation\Auth\ThrottlesLogins;
 
 class LoginController extends Controller
 {
+    use ThrottlesLogins;
+
+    /**
+     * Number of login attempts before the user is locked out.
+     *
+     * @var integer
+     */
+    protected $maxAttempts = 5;
+
+    /**
+     * Lockout duration.
+     *
+     * @var integer
+     */
+    protected $decayMinutes = 1;
+
     /**
      * Log the user in.
      *
@@ -16,9 +36,17 @@ class LoginController extends Controller
      */
     public function __invoke(LoginRequest $request)
     {
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
         $token = auth()->attempt($request->only('email', 'password'));
 
         if (!$token) {
+            $this->incrementLoginAttempts($request);
+
             return response([
                 'errors' => [
                     'email' => __('auth.failed')
@@ -32,5 +60,45 @@ class LoginController extends Controller
                     'token' => $token
                 ]
             ]);
+    }
+
+    /**
+     * Redirect the user after determining they are locked out.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        return response([
+            'errors' => [
+                'email' => Lang::get('auth.throttle', ['seconds' => $seconds]),
+            ]
+        ], 429);
+    }
+
+    /**
+     * Get the throttle key for the given request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return string
+     */
+    protected function throttleKey(Request $request)
+    {
+        return Str::lower($request->input($this->username())) . '|' . $request->ip();
+    }
+
+    /**
+     * Get the login username to be used by the controller.
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'email';
     }
 }
