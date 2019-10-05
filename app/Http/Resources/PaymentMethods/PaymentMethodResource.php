ed96603bcd8a3dc27dd250da4742f8e33a3c33d9
Pixlforge <celien@pixlforge.ch>
2019-10-05 12:29:10 +0200

    Add created_at property

--- app/Http/Resources/PaymentMethods/PaymentMethodResource.php
+++ app/Http/Resources/PaymentMethods/PaymentMethodResource.php
@@ -19,7 +19,13 @@ class PaymentMethodResource extends JsonResource
             'card_type' => $this->card_type,
             'card_type_slug' => $this->card_type_slug,
             'last_four' => $this->last_four,
-            'is_default' => $this->isDefault()
+            'is_default' => $this->isDefault(),
+            'created_at' => [
+                'en' => $this->created_at->locale('en')->longAbsoluteDiffForHumans(),
+                'fr' => $this->created_at->locale('fr')->longAbsoluteDiffForHumans(),
+                'de' => $this->created_at->locale('de')->longAbsoluteDiffForHumans(),
+                'it' => $this->created_at->locale('it')->longAbsoluteDiffForHumans(),
+            ]
         ];
     }
 }
